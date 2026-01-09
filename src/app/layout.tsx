740539ba9f083730ec9000530e6117add69633cc
arjun <arjunjhukal.makura@gmail.com>
2026-01-09 09:59:49 +0545

    updated the minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ const metadata: Metadata = {
     icon: "/assets/images/logo.png",
   },
 };
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+const SITE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL!;
+
 export async function generateMetadata(): Promise<Metadata> {
   try {
     const response = await pageSEO();
@@ -48,6 +52,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {GTM_ID && (
+          <Script
+            id="gtm-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
+        )}
+      </head>
       {/* className="dark" */}
       <body className={`${inter.className} scroll-smooth`} cz-shortcut-listen="true">
         <SeonProvider>
